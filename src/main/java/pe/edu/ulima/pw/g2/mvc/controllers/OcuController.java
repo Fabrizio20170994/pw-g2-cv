dae2881bfba6b4605affb3ffe96466315ace07b8
Diego <diegoantoniodiegodie@gmail.com>
2020-07-16 18:21:31 -0500

    ocupaciones controller sin errores

--- src/main/java/pe/edu/ulima/pw/g2/mvc/controllers/OcuController.java
+++ src/main/java/pe/edu/ulima/pw/g2/mvc/controllers/OcuController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import pe.edu.ulima.pw.g2.mvc.dao.entities.OcupationEntity;
 import pe.edu.ulima.pw.g2.mvc.dao.repositories.OcupationRepository;
 
-@Controller("/ocupaciones")
+@Controller
 public class OcuController {
     
     @Autowired
     private OcupationRepository repoOcu;
 
-    @RequestMapping(value = "/ocupaciones", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping("/ocupac")
     public String cargarOcupaciones(Model model){
         List<OcupationEntity> listaOcu = repoOcu.findAll();
         model.addAttribute("listaOcupaciones", listaOcu);
