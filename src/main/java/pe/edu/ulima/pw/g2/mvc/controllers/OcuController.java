7890591f18decc5374d5c9cdf6fef8663c8a7b80
Diego <diegoantoniodiegodie@gmail.com>
2020-07-16 16:04:33 -0500

    ocupaciones controller recien creado

---
+++ src/main/java/pe/edu/ulima/pw/g2/mvc/controllers/OcuController.java
@@ -0,0 +1,29 @@
+package pe.edu.ulima.pw.g2.mvc.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import pe.edu.ulima.pw.g2.mvc.dao.entities.OcupationEntity;
+import pe.edu.ulima.pw.g2.mvc.dao.repositories.OcupationRepository;
+
+@Controller("/ocupaciones")
+public class OcuController {
+    
+    @Autowired
+    private OcupationRepository repoOcu;
+
+    @RequestMapping(value = "/ocupaciones", method = RequestMethod.GET)
+    @ResponseBody
+    public String cargarOcupaciones(Model model){
+        List<OcupationEntity> listaOcu = repoOcu.findAll();
+        model.addAttribute("listaOcupaciones", listaOcu);
+        return "ocupaciones";
+    }
+
+}
