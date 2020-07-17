9af699e4cc4a1d7d33ad389914cf28def77e9c93
Diego <diegoantoniodiegodie@gmail.com>
2020-07-16 19:07:52 -0500

    avance controller

--- src/main/java/pe/edu/ulima/pw/g2/mvc/controllers/OcuController.java
+++ src/main/java/pe/edu/ulima/pw/g2/mvc/controllers/OcuController.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import pe.edu.ulima.pw.g2.mvc.dao.entities.OcupationEntity;
 import pe.edu.ulima.pw.g2.mvc.dao.repositories.OcupationRepository;
@@ -19,7 +16,7 @@ public class OcuController {
     @Autowired
     private OcupationRepository repoOcu;
 
-    @GetMapping("/ocupac")
+    @GetMapping("/ocupaciones")
     public String cargarOcupaciones(Model model){
         List<OcupationEntity> listaOcu = repoOcu.findAll();
         model.addAttribute("listaOcupaciones", listaOcu);
