c7e25fbc2cbc0cf982303cc95613673b5a23269d
Diego <diegoantoniodiegodie@gmail.com>
2020-07-20 01:00:08 -0500

    creando condiciones

--- src/main/resources/static/js/experiencias.js
+++ src/main/resources/static/js/experiencias.js
@@ -1,3 +1,8 @@
 function redireccionar(entidadId){
     location.href="/actualizar-crear-experiencia/" + entidadId;
 }
+
+function verifacacionCrearExperiencia(){
+    error=1;
+    var nombre=document.getElementById("logro");
+}
